2716e7d9fa630b4cba668f4ece39b768e13dc010
HasaMahawatta <92071475+HasaMahawatta@users.noreply.github.com>
2023-03-13 21:35:53 +1100

    dataBase migration

---
+++ OnlineBookStore/Migrations/202303131031310_Initial.cs
@@ -0,0 +1,41 @@
+namespace OnlineBookStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        BookID = c.String(nullable: false, maxLength: 128),
+                        BookTitle = c.String(),
+                        IsReserved = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.BookID);
+            
+            CreateTable(
+                "dbo.ReserveDetails",
+                c => new
+                    {
+                        ReserveId = c.String(nullable: false, maxLength: 128),
+                        BookId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.ReserveId)
+                .ForeignKey("dbo.Books", t => t.BookId)
+                .Index(t => t.BookId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.ReserveDetails", new[] { "BookId" });
+            DropForeignKey("dbo.ReserveDetails", "BookId", "dbo.Books");
+            DropTable("dbo.ReserveDetails");
+            DropTable("dbo.Books");
+        }
+    }
+}
